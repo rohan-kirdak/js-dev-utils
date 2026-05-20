da716b48d8cf230ca15e4805ce135cf9ada7809e
Rohan <rohankirdak8756@gmail.com>
2026-05-20 11:25:51 +0000

    docs: fix typo in function description

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -54,3 +54,10 @@ function getDayName(date = new Date()) {
 function nowReadable() {
   return new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
 }
+
+/**
+ * Returns the day name (Monday, Tuesday...)
+ */
+function getDayName(date = new Date()) {
+  return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
+}
