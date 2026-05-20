dd447db703d71347f64c8d606d997a7c078f82d4
Rohan <rohankirdak8756@gmail.com>
2026-05-20 08:01:42 +0000

    refactor: clean up string utils module

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -34,3 +34,14 @@ function randomPick(arr) {
 }
 
 module.exports = { unique, chunk, flattenOne, randomPick };
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
