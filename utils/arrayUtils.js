dfa18b17c91b97b02327b857194a5fa860a10eb2
Rohan <rohankirdak8756@gmail.com>
2026-06-05 09:21:53 +0000

    refactor: clean up string utils module

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -167,3 +167,14 @@ function groupBy(arr, key) {
     return acc;
   }, {});
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
