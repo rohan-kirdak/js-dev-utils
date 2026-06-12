6ad9e819c615ddfae7dbf3138d2af8cc425f9d5d
Rohan <rohankirdak8756@gmail.com>
2026-06-12 08:44:53 +0000

    feat: add isPast date utility

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -242,3 +242,14 @@ function shuffle(arr) {
   }
   return arr;
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
