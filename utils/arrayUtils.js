068e60513863eec63ab7b2cf05d4933a95a2224f
Rohan <rohankirdak8756@gmail.com>
2026-06-14 08:38:14 +0000

    feat: add isPast date utility

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -267,3 +267,13 @@ function isEmpty(arr) {
 function minVal(arr) {
   return Math.min(...arr);
 }
+
+/**
+ * Groups array items by a key
+ */
+function groupBy(arr, key) {
+  return arr.reduce((acc, item) => {
+    (acc[item[key]] = acc[item[key]] || []).push(item);
+    return acc;
+  }, {});
+}
