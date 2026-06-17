bcac475b57c727797a923481434d98d718ad0c5e
Rohan <rohankirdak8756@gmail.com>
2026-06-17 08:56:40 +0000

    feat: add isPast date utility

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -225,3 +225,10 @@ function stripSpecialChars(str) {
 function reverseStr(str) {
   return str.split('').reverse().join('');
 }
+
+/**
+ * Counts words in a string
+ */
+function wordCount(str) {
+  return str.trim().split(/\s+/).length;
+}
