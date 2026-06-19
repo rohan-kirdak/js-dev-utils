cb49a40d7c21726adf9ea9d32b70661222361808
Rohan <rohankirdak8756@gmail.com>
2026-06-19 10:01:44 +0000

    refactor: simplify array flatten logic

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -246,3 +246,10 @@ function zeroPad(num, size = 2) {
 function wordCount(str) {
   return str.trim().split(/\s+/).length;
 }
+
+/**
+ * Pads a number with leading zeros
+ */
+function zeroPad(num, size = 2) {
+  return String(num).padStart(size, '0');
+}
