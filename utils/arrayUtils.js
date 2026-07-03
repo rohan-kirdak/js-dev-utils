b013332e9b7e3d67f7a81e3b916a4c3281ed681d
Rohan <rohankirdak8756@gmail.com>
2026-07-03 08:36:10 +0000

    refactor: remove redundant null checks

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -392,3 +392,14 @@ function isEmpty(arr) {
 function maxVal(arr) {
   return Math.max(...arr);
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
