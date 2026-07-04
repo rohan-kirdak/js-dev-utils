9d32888a71c1bd3640c99d34b9ad30ae53f474b5
Rohan <rohankirdak8756@gmail.com>
2026-07-04 07:14:53 +0000

    fix: handle null/undefined edge case

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -254,3 +254,10 @@ function addDays(date, n) {
 function isPast(date) {
   return new Date(date) < new Date();
 }
+
+/**
+ * Returns the day name (Monday, Tuesday...)
+ */
+function getDayName(date = new Date()) {
+  return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
+}
