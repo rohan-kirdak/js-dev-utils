31756a76baac0e571c69411eeca470c7354b233f
Rohan <rohankirdak8756@gmail.com>
2026-07-06 09:05:20 +0000

    refactor: clean up string utils module

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -275,3 +275,10 @@ function getDayName(date = new Date()) {
 function nowIST() {
   return new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
 }
+
+/**
+ * Returns the day name (Monday, Tuesday...)
+ */
+function getDayName(date = new Date()) {
+  return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
+}
