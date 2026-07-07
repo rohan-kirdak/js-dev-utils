c4af9c02777fe8d66e0dda29683499da3d95d45d
Rohan <rohankirdak8756@gmail.com>
2026-07-07 07:27:43 +0000

    refactor: remove redundant null checks

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -282,3 +282,10 @@ function nowIST() {
 function getDayName(date = new Date()) {
   return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
 }
+
+/**
+ * Returns current IST timestamp as readable string
+ */
+function nowIST() {
+  return new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+}
