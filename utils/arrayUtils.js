74e93afd381126c809cd862e570507a6b28b515c
Rohan <rohankirdak8756@gmail.com>
2026-07-10 08:54:49 +0000

    feat: add date formatting helper

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -456,3 +456,13 @@ function shuffle(arr) {
   }
   return arr;
 }
+
+/**
+ * Groups array items by a key
+ */
+function groupBy(arr, key) {
+  return arr.reduce((acc, item) => {
+    (acc[item[key]] = acc[item[key]] || []).push(item);
+    return acc;
+  }, {});
+}
