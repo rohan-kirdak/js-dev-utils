416f90be1258a4a0267bc4a3ae6a817016a6536e
Rohan <rohankirdak8756@gmail.com>
2026-07-23 07:56:06 +0000

    feat: add new string helper util

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -588,3 +588,14 @@ function sum(arr) {
 function minVal(arr) {
   return Math.min(...arr);
 }
+
+/**
+ * Shuffles an array in place
+ */
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
