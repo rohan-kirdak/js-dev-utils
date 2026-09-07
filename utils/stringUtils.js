846077f363106c2d1b6ed5b4b3a4124a9fd30a30
Rohan <rohankirdak8756@gmail.com>
2026-09-07 12:31:15 +0000

    fix: correct regex in validator

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -694,3 +694,10 @@ function isNumeric(str) {
 function toSnakeCase(str) {
   return str.replace(/[A-Z]/g, l => '_' + l.toLowerCase());
 }
+
+/**
+ * Removes special characters from string
+ */
+function stripSpecialChars(str) {
+  return str.replace(/[^a-zA-Z0-9 ]/g, '');
+}
