75c68b734a1b6b170d04c8220925cae30966dd05
Rohan <rohankirdak8756@gmail.com>
2026-09-09 08:50:47 +0000

    docs: add JSDoc comments to utils

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -679,3 +679,10 @@ function getDayName(date = new Date()) {
 function getDayName(date = new Date()) {
   return new Date(date).toLocaleDateString('en-IN', { weekday: 'long' });
 }
+
+/**
+ * Checks if a date is in the past
+ */
+function isPast(date) {
+  return new Date(date) < new Date();
+}
