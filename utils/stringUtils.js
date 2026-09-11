84b94c04799daf2ec33566b1b223446f2f661567
Rohan <rohankirdak8756@gmail.com>
2026-09-11 11:46:15 +0000

    feat: add array groupBy utility

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -701,3 +701,10 @@ function toSnakeCase(str) {
 function stripSpecialChars(str) {
   return str.replace(/[^a-zA-Z0-9 ]/g, '');
 }
+
+/**
+ * Converts camelCase to snake_case
+ */
+function toSnakeCase(str) {
+  return str.replace(/[A-Z]/g, l => '_' + l.toLowerCase());
+}
