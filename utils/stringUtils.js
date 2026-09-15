fcaf19e3bf7148a6da7551ad3371586c672e5b4b
Rohan <rohankirdak8756@gmail.com>
2026-09-15 09:38:07 +0000

    feat: add isPast date utility

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -715,3 +715,10 @@ function toSnakeCase(str) {
 function zeroPad(num, size = 2) {
   return String(num).padStart(size, '0');
 }
+
+/**
+ * Counts words in a string
+ */
+function wordCount(str) {
+  return str.trim().split(/\s+/).length;
+}
