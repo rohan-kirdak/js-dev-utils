af6cf963099f4f996150f89818d40093faf70cdb
Rohan <rohankirdak8756@gmail.com>
2026-09-17 10:20:36 +0000

    fix: correct regex in validator

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -1000,3 +1000,10 @@ function shuffle(arr) {
 function isEmpty(arr) {
   return !arr || arr.length === 0;
 }
+
+/**
+ * Returns min value in an array
+ */
+function minVal(arr) {
+  return Math.min(...arr);
+}
