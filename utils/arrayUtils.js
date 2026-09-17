f0ac67fce1387257108f3392233a6822585105f0
Rohan <rohankirdak8756@gmail.com>
2026-09-17 11:12:36 +0000

    feat: add isEmpty array check

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -1007,3 +1007,10 @@ function isEmpty(arr) {
 function minVal(arr) {
   return Math.min(...arr);
 }
+
+/**
+ * Sums all numbers in an array
+ */
+function sum(arr) {
+  return arr.reduce((a, b) => a + b, 0);
+}
