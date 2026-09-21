2e566e3cbf6aa772a42d3ffd605ee63f799e610e
Rohan <rohankirdak8756@gmail.com>
2026-09-21 11:31:30 +0000

    docs: fix typo in function description

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -743,3 +743,10 @@ function reverseStr(str) {
 function wordCount(str) {
   return str.trim().split(/\s+/).length;
 }
+
+/**
+ * Reverses a string
+ */
+function reverseStr(str) {
+  return str.split('').reverse().join('');
+}
